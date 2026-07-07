118adfe0c79db9de63288b9f2b3a432859224762
Ayman Ragab <aymanrar2c@gmail.com>
2026-07-07 23:10:29 +0300

    fix(pipa): disable sde custom client mode for standard Linux DRM clients
    Switch sdecustom default from true to false so that stage reassignment in _sde_crtc_check_zpos is enabled for non-Android clients (Plymouth, Weston, Phosh). Fixes repeated '> 2 planes @ stage 0' errors during boot splash handoff and allows the continuous splash pipe to be released cleanly.

--- techpack/display/msm/sde/sde_kms.c
+++ techpack/display/msm/sde/sde_kms.c
@@ -101,7 +101,7 @@ static const char * const iommu_ports[] = {
  *
  *	This is disabled by default.
  */
-static bool sdecustom = true;
+static bool sdecustom = false;
 module_param(sdecustom, bool, 0400);
 MODULE_PARM_DESC(sdecustom, "Enable customizations for sde clients");
 
